7b695ff6b37e3911e26ac9c01dfb117740b7ee6c
Jamie Hardesty <jamiehardesty@outlook.com>
2021-03-08 17:52:17 -0800

    Formatted with Prettier

--- public/src/books.js
+++ public/src/books.js
@@ -1,52 +1,38 @@
 // Note: Please do not change the name of the functions. The tests use those names to validate your code.
 
 function findAuthorById(authors, id) {
-  return authors.find(
-    (auth) => auth.id == id
-  );
+  return authors.find((auth) => auth.id == id);
 }
 
 function findBookById(books, id) {
-  return books.find(
-    (book) => book.id == id
-  );
+  return books.find((book) => book.id == id);
 }
 
 function partitionBooksByBorrowedStatus(books) {
   let partition = [];
   partition.push(
-    books.filter( // [0] is required !!
-      ({borrows}) => !borrows[0].returned
+    books.filter(// [0] is required !!
+      ({ borrows }) => !borrows[0].returned
     )
   );
-  partition.push(
-    books.filter(
-      ({borrows}) => borrows[0].returned
-    )
-  );
+  partition.push(books.filter(({ borrows }) => borrows[0].returned));
   return partition;
 }
 
 function getBorrowersForBook(book, accounts) {
-  // TA help:
-  const acctID = accounts.reduce(
-    (acc, account)=>{
-      acc[account.id] = account;
-      return acc;
-    },{}
-  )
+  const acctID = accounts.reduce((acc, account) => {
+    acc[account.id] = account;
+    return acc;
+  }, {});
   return book.borrows
-  .map(
-    ({id, returned})=>(
-      {
-        ...acctID[id],
-        returned
-      }
-    )
-  )
-  .slice(0, 10);
-  //
-  // FORMER SOLUTION: 
+    .map(({ id, returned }) => ({
+      ...acctID[id],
+      returned,
+    }))
+    .slice(0, 10);
+}
+
+  // FORMER SOLUTION:
   //
   // return book.borrows
   //   .map(
@@ -58,7 +44,6 @@ function getBorrowersForBook(book, accounts) {
   //     }
   //   )
   //   .slice(0, 10);
-}
 
 module.exports = {
   findAuthorById,
--- public/src/home.js
+++ public/src/home.js
@@ -9,71 +9,43 @@ function getTotalAccountsCount(accounts) {
 }
 
 function getBooksBorrowedCount(books) {
-  return borrowed = books
-    .filter(
-      ({ borrows }) => !borrows[0].returned
-    )
-    .length; // [0] is necessary !!
+  return (borrowed = books.filter(({ borrows }) => !borrows[0].returned)
+    .length); // [0] is necessary !!
 }
 
-function getMostCommonGenres(books) { // try using .reduce() for a smaller f(x) 
+function getMostCommonGenres(books) {// try using .reduce()
   let arr = []; // this establishes the shape
   for (let book in books) {
     let firstRun = false;
-    for (let idx in arr){
+    for (let idx in arr) {
       if (books[book].genre === arr[idx].name) {
         firstRun = true;
         arr[idx].count++;
       }
-    } 
-    // below line pushes the appropriate shape
+    }// below line pushes the appropriate shape
     if (firstRun === false) {
-      arr.push(
-        { name: books[book].genre, count: 1 }
-      );
+      arr.push({ name: books[book].genre, count: 1 });
     }
-    
   }
-  return arr
-    .sort(
-      (a, b) => (a.count > b.count ? -1 : 1)
-    )
-    .slice(0, 5); 
+  return arr.sort((a, b) => (a.count > b.count ? -1 : 1)).slice(0, 5);
 }
 
 function getMostPopularBooks(books) {
-  let arr = []; 
-  // for (let i = 0; i < books.length; i++) {
+  let arr = [];
   for (let book in books) {
-    arr.push(
-      { name: books[book].title, count: books[book].borrows.length }
-    );
+    arr.push({ name: books[book].title, count: books[book].borrows.length });
   }
-  return arr
-    .sort(
-      (a, b) => (a.count > b.count ? -1 : 1)
-    )
-    .slice(0, 5); 
+  return arr.sort((a, b) => (a.count > b.count ? -1 : 1)).slice(0, 5);
 }
 
 function getMostPopularAuthors(books, authors) {
   let arr = []; // this establishes the shape
-  // for (let i = 0; i < books.length; i++) {
-  for (let book in books){
-    let authBook = books[book].authorId;
-    let authMatch = authors.find(
-      (auth) => auth.id === authBook
-    );
+  for (let book in books) {
+    let authMatch = authors.find((auth) => auth.id === books[book].authorId);
     let authName = authMatch.name.first + " " + authMatch.name.last;
-    arr.push(
-      { name: authName, count: books[book].borrows.length }
-    );
+    arr.push({ name: authName, count: books[book].borrows.length });
   }
-  return arr
-    .sort(
-      (a, b) => (a.count > b.count ? -1 : 1)
-    )
-    .slice(0, 5);
+  return arr.sort((a, b) => (a.count > b.count ? -1 : 1)).slice(0, 5);
 }
 
 module.exports = {
